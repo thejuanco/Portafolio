c45945ea69192f6bc9eb4439657ec0243415dfea
Juanco <juanffo874@gmail.com>
2025-07-25 19:54:15 -0600

    Instala la fuente descargada y configura los archivos necesarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import './App.css'
+
 function App() {
   return (
     <>
       <div className="text-center">
-        Hola
+        <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl">
+          Hola, soy{" "}
+          <span className="bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
+            Juanco
+          </span>
+        </h1>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
