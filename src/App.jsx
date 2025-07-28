b8880e14532783dc8b5b204a5220507e72d81e7c
Juanco <juanffo874@gmail.com>
2025-07-27 23:47:55 -0600

    Añade etiquetas links para la navegacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Link } from "react-router";
 
 function App() {
   const projects = [
@@ -116,8 +117,12 @@ function App() {
                 Descargar CV
               </button>
             </div>
-             <div className="flex items-center space-x-4">
-              Botones de redes sociales
+             <div className="flex items-center justify-center space-x-4">
+              <Link to="#" className="p-2 rounded-lg hover:bg-gray-100">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                </svg>
+              </Link>
              </div>
           </div>
         </div>
