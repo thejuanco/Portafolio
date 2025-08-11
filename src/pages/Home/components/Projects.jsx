1bdb5c34480081e5a9de9d1a4dc7f95a0e304437
Juanco <juanffo874@gmail.com>
2025-08-11 12:51:46 -0600

    Agrega una nueva seccion, que contiene proyectos secundarios

--- src/pages/Home/components/Projects.jsx
+++ src/pages/Home/components/Projects.jsx
@@ -61,6 +61,38 @@ const Projects = () => {
         ))
         }
         </div>
+        <div className="space-y-6">
+          <h3 className="text-2xl font-bold text-center">Otros Proyectos</h3>
+          <div className="grid gap-6 md:grid-cols-2 mx-8">
+            {projects.filter(project => !project.featured).map((project, index) => (
+              <div key={index} className="border border-gray-300 rounded-lg p-5">
+                <div className="flex flex-row justify-between space-y-2">
+                  <h2 className="font-medium text-lg">{project.title}</h2>
+                    <div className="space-x-2">
+                      <button className="border border-gray-200 px-2 py-1 rounded-full">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                      </svg>
+                      </button>
+                      <button className="border border-gray-200 px-2 py-2 rounded-full">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5" />
+                        </svg>
+                      </button>
+                    </div>
+                </div>
+                <p className="text-gray-500">{project.description}</p>
+                    <div className="flex flex-wrap gap-2 mt-2">
+                      {project.technologies.map((tech, techIndex) => (
+                        <div key={techIndex} className="px-2 border border-gray-200 rounded-full">
+                          {tech}
+                        </div>
+                      ))}
+                    </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </section>
   );
