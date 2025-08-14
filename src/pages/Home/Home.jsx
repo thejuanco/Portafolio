a52670235d567b185a3b8324d07adebfaadbe56c
Juanco <juanffo874@gmail.com>
2025-08-14 14:48:34 -0600

    Se agregan iconos y se redirecciona al formulario de contacto

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -44,17 +44,36 @@ const Home = () => {
               </h1>
               <p className="mx-auto max-w-[700px] text-lg text-gray-500 sm:text-xl mt-6">
                 Desarrollador Full Stack apasionado por crear experiencias
-                digitales excepcionales. Con experiencia en React, Next.js, .NET, C#, SQL Server y
-                tecnologías modernas.
+                digitales excepcionales. Con experiencia en React, Next.js,
+                .NET, C#, SQL Server y tecnologías modernas.
               </p>
             </div>
             <div className="flex flex-col items-center justify-center sm:flex-row gap-4">
               <Link
                 to="https://github.com/thejuanco"
-                className="bg-gray-800 text-white font-medium py-2 px-4 block md:inline-block rounded-full hover:bg-gray-900 transition-transform duration-300 transform hover:scale-105"
+                className="flex items-center bg-gray-800 text-white font-medium py-2 px-4 rounded-full hover:bg-gray-900 transition-transform duration-300 transform hover:scale-105"
                 target="_blank"
               >
-                Ver Perfil de Github
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="size-6 mr-1"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                  />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                </svg>
+                Ver Perfil en Github
               </Link>
               <button className="flex items-center gap-2 bg-gray-200 text-slate-900 font-medium py-2 px-4 rounded-full hover:bg-gray-300 transition-transform duration-300 transform hover:scale-105">
                 <svg
@@ -95,10 +114,10 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <Projects/>
-      <SkillsAndTech/>
-      <Contact/>
-      <Footer/>
+      <Projects />
+      <SkillsAndTech />
+      <Contact />
+      <Footer />
     </div>
   );
 };
--- src/pages/Home/components/Contact.jsx
+++ src/pages/Home/components/Contact.jsx
@@ -13,14 +13,18 @@ const Contact = () => {
           </p>
         </div>
         <div className="flex flex-col sm:flex-row gap-4 justify-center">
-            <button className="bg-gray-900 text-slate-200 py-2 px-4 rounded-full hover:bg-gray-800">
+            <Link to="/contact" className="flex items-center font-medium bg-gray-900 text-slate-200 py-2 px-4 rounded-full hover:bg-gray-800">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 mr-1">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+                </svg>
                 Enviar Correo
-            </button>
+            </Link>
             <Link 
-              className="flex items-center gap-2 border border-gray-300 hover:bg-gray-100 text-slate-900 py-2 px-4 rounded-full"
+              className="flex items-center gap-2 border border-gray-300 font-medium hover:bg-gray-100 text-slate-900 py-2 px-4 rounded-full"
               to="https://www.linkedin.com/in/juan-cruz-ortiz-b56607283/"
               target="_blank"
             >
+              <img src="/images/linkedin.png" alt="Descripción de la imagen" className="w-7 h-7"/>
                 LinkedIn
             </Link>
         </div>
